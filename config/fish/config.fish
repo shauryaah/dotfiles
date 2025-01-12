798d79791d0876abcb5ced930ba567b5f669e97b
Shaurya Prabhakar <181096592+shauryaah@users.noreply.github.com>
2025-01-12 22:58:37 +0530

    Uploading before nuking my system 🫠

---
+++ config/fish/config.fish
@@ -0,0 +1,15 @@
+if status is-interactive
+	fastfetch	
+end
+
+set -g fish_color_user f99f94
+
+alias whereami=pwd
+
+alias gtop="sudo intel_gpu_top"
+
+alias qs="sudo apt install"
+alias qss="sudo apt update"
+alias qr="sudo apt remove"
+
+fish_add_path /home/sagiri/.spicetify
